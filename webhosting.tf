e8d7217964b7a4de9273a02e1a14458202e34a66
Lewis Desforges <lewis.desforges@gmail.com>
2023-03-21 20:58:15 +0000

    Switch bucket ACL to policy

--- webhosting.tf
+++ webhosting.tf
@@ -3,9 +3,24 @@ resource "aws_s3_bucket" "hosting_bucket" {
 
 }
 
-resource "aws_s3_bucket_acl" "hosting_bucket_acl" {
+resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
   bucket = aws_s3_bucket.hosting_bucket.id
-  acl    = "public-read"
+  policy = <<POLICY
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:GetObject",
+          "Resource" : [
+            "arn:aws:s3:::${aws_s3_bucket.hosting_bucket.id}/*",
+            "arn:aws:s3:::${aws_s3_bucket.hosting_bucket.id}"
+          ]
+        }
+      ]
+    }
+  POLICY
 }
 
 resource "aws_s3_object" "indexhtml" {
